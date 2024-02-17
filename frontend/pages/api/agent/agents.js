3442dbdc1cedcbe580aaf4099c36cea7b1adc19b
parnavh <parnav100sach@gmail.com>
2024-02-17 22:34:00 +0530

    feat(api): aggregation for main dashboard

--- frontend/pages/api/agent/agents.js
+++ frontend/pages/api/agent/agents.js
@@ -3,5 +3,14 @@ import { prisma } from "@/lib/db";
 export default async function handler(req, res) {
   const agents = await prisma.agents.findMany();
 
+  const promises = []
+
+  for (let i = 0; i < agents.length; i++) {
+    const a = agents[i];
+    promises.push(fetch(`http://${a.ip}:${a.port}/containers/status`).then(res => res.json()).then(data => agents[i].containers = data))
+  }
+
+  await Promise.all(promises);
+
   res.send(agents);
 }

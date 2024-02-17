4e044a6d5361762fa50a1d938d961e15f29c16c8
Rushabh Javeri <javeri.rushabh45@gmail.com>
2024-02-17 21:34:26 +0530

    Added agent detail api route

---
+++ frontend/pages/api/agent/detail.js
@@ -0,0 +1,19 @@
+import { prisma } from '@/lib/db';
+
+export default async function handler(req, res) {
+  const { agent, containerId } = req.query;
+
+  const { name, ip, port } = await prisma.agents.findUnique({
+    where: {
+      id: parseInt(agent),
+    },
+  });
+
+  const detail = await fetch(
+    `http://${ip}:${port}/containers/stats/${containerId}`
+  );
+
+  const stats = await detail.json();
+
+  res.send(stats);
+}
